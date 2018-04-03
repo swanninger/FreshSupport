0450a29c08966077879a07ae9236d896d01bcb85
Scott Wanninger <swanninger@gmail.com>
2018-04-03 15:18:09 -0500

    EOD

---
+++ src/main/java/net/freshservers/support/commands/EmailCommand.java
@@ -0,0 +1,12 @@
+package net.freshservers.support.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailCommand {
+
+}
--- src/main/java/net/freshservers/support/controllers/EmailController.java
+++ src/main/java/net/freshservers/support/controllers/EmailController.java
@@ -1,4 +1,17 @@
 package net.freshservers.support.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class EmailController {
+
+    public EmailController() {
+    }
+
+    @RequestMapping("/requestform")
+    public String getRequestForm(){
+
+        return "requestform";
+    }
 }
