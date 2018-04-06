75045e0b39419bf6a2948381e2243c60d8699701
Megatron\PhazedOut <swanninger@gmail.com>
2018-04-06 16:58:21 -0500

    updates

--- src/main/java/net/freshservers/support/commands/EmailCommand.java
+++ src/main/java/net/freshservers/support/commands/EmailCommand.java
@@ -9,4 +9,15 @@ import lombok.Setter;
 @NoArgsConstructor
 public class EmailCommand {
 
+    private String userName;
+    private String concept;
+    private String location;
+
+    private String reqName;
+    private String reqEmail;
+    private String reqPosition;
+
+    private String reqType;
+
+    private String toolType;
 }
