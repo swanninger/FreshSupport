9ff5f73da1d542c058593213b8b6ce95d7d94499
Megatron\PhazedOut <swanninger@gmail.com>
2021-05-06 10:33:18 -0500

    testing git with new IDE version

--- src/main/java/net/freshservers/support/configuration/ApplicationSecurity.java
+++ src/main/java/net/freshservers/support/configuration/ApplicationSecurity.java
@@ -43,7 +43,6 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/form/recipe").hasAnyAuthority("GM_USER", "OWNER", "OPERATOR", "ADMIN")
                 .antMatchers("/mac").hasAuthority("MAC")
                 .antMatchers("/tech").hasAuthority("OWNER")
-                .antMatchers("/resources/tech").hasAuthority("OWNER")
                 .antMatchers(("/cke")).hasAuthority("CKE")
                 .anyRequest().fullyAuthenticated()
                 .and().formLogin().loginPage("/login").permitAll()
